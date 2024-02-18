a0f73be49276d48d565648f2d1238c6879b7657d
Ranjith <115684861+Ranjith2804@users.noreply.github.com>
2024-02-18 17:53:25 +0530

    55.Jump Game.py

---
+++ 55.Jump Game.py
@@ -0,0 +1,7 @@
+class Solution(object):
+    def canJump(self, nums):
+        last=len(nums)-1
+        for i in range(len(nums)-2,-1,-1):
+            if(nums[i]+i >= last):
+                last=i
+        return last==0
