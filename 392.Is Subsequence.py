ec38a1607ac22990cafae3a37a50dfacc95b1b0f
Ranjith <115684861+Ranjith2804@users.noreply.github.com>
2024-02-20 20:33:59 +0530

    392.Is Subsequence.py

---
+++ 392.Is Subsequence.py
@@ -0,0 +1,7 @@
+class Solution(object):
+    def isSubsequence(self, s, t):
+        j=0
+        for i in range(len(t)):
+            if j < len(s) and s[j] == t[i]:
+                j += 1
+        return j==len(s)
