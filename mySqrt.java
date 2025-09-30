ee05276792696f184e9127891ee394a0628632f8
Ranjith <115684861+Ranjith2804@users.noreply.github.com>
2025-09-30 21:59:52 +0530

    Create mySqrt.java

---
+++ mySqrt.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int mySqrt(int x) {
+        if(x==0) return x;
+        int l=1,r=x,a=0;
+        while(l<=r){
+            int mid=l+(r-l)/2;
+            long sq=(long)mid*mid;
+            if(sq==x) return mid;
+            else if(sq<x){a=mid;l=mid+1;}
+            else r=mid-1;
+        }
+        return a;
+    }
+}
