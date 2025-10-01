e3bd144bbd884b0e2bc394573bb4d3b133c306bb
Ranjith <115684861+Ranjith2804@users.noreply.github.com>
2025-10-02 00:16:07 +0530

    Update majorityElement.java

--- majorityElement.java
+++ majorityElement.java
@@ -1,6 +1,22 @@
+// class Solution {
+//     public int majorityElement(int[] nums) {
+//         Arrays.sort(nums);
+//         return nums[nums.length/2];
+//     }
+// }
 class Solution {
-    public int majorityElement(int[] nums) {
-        Arrays.sort(nums);
-        return nums[nums.length/2];
+    public int majorityElement(int[] n) {
+        int c=0,c1=0;
+        for(int i=0;i<n.length;i++){
+            if(c1==0){
+                c=n[i];
+                c1=1;
+            }
+            else{
+                if(c==n[i]) c1++;
+                else c1--;
+            }
+        }
+        return c;
     }
 }
