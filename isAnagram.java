627a9209291a146c22697053d1f7c1908621d160
Ranjith <115684861+Ranjith2804@users.noreply.github.com>
2025-10-14 16:40:58 +0530

    Create isAnagram.java

---
+++ isAnagram.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        char []a=s.toCharArray();
+        char []b=t.toCharArray();
+        Arrays.sort(a);
+        Arrays.sort(b);
+        String c=new String(a);
+        String d=new String(b);
+        return c.equals(d);
+    }
+}
